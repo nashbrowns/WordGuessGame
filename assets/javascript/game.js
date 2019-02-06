fe769e298a6f0b07f0f94cc1316993255008f8ae
Nash McDonald <nash42192@gmail.com>
2019-02-05 18:51:51 -0800

    added loop that finds all indices of word in array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
 var SciWords = ['dune', 'riker', 'darth vader', 'android', 'phillip k dick', 'cyberspace', 'alien']; /* Initializing Sci-Fi Word Bank */
+var CorrectGuess = [];
+var IncorrectGuess = [];
 
 var SciChoice = SciWords[Math.floor(Math.random() * SciWords.length)]; /* Randomly chooses word from SciWords array */
 
@@ -6,12 +8,27 @@ console.log('SciChoice = '+SciChoice);
 
 WordTemp = GenBlank(SciChoice);
 
+console.log(typeof WordTemp);
 
 document.getElementById("Word-Temp").innerHTML = WordTemp;
 
-console.log(WordTemp.length);
+document.onkeyup = function(event) {
+    if( (Alphanumeric(event.key)) && (CorrectGuess.indexOf(event.key) == -1) && (IncorrectGuess.indexOf(event.key) == -1)){
 
+        var Guess = event.key.toLowerCase();
+        
+        if( SciChoice.indexOf(Guess) !== -1 ){
+            CorrectGuess.push(Guess);
+            console.log(CorrectGuess);
+            UpdateBoard(Guess);
+        }
+        else{
+            IncorrectGuess.push(Guess);
+            console.log(IncorrectGuess);
+        }
 
+    }
+  };
 
 /* --------------------------------------------------------------------------------------------------------- */
 /* ------------------------------------------------ Functions ---------------------------------------------- */
@@ -36,6 +53,42 @@ function GenBlank(Word){
 
     return BlankArr;
 }
+
+function Alphanumeric(Letter){ 
+    var Alpha = /^[a-zA-Z]+$/;
+    if(Letter.match(Alpha)){ 
+        return true; 
+    } 
+    else {  
+        return false; 
+    } 
+}
+
+function UpdateBoard(Letter){
+    var indices = [];
+    var StrCon = ""; /* Used to convert back to string */
+
+    WordTemp = WordTemp.split(""); /* Splitting into array so can replace elements */
+
+    /* Finds all instances of letter and puts them in an arrray called indices */
+    var idx = SciChoice.indexOf(Letter);
+    while(idx !== -1){
+        indices.push(idx);
+        idx = SciChoice.indexOf(Letter, idx + 1);
+    }
+
+
+    WordTemp[SciChoice.indexOf(Letter)] = Letter; /* Putting correctly guessed letter into displayed word */
+    
+    for(var i = 0; i<WordTemp.length; i++){
+        StrCon = StrCon + WordTemp[i];
+        }
+        
+    WordTemp = StrCon;
+    
+    document.getElementById("Word-Temp").innerHTML = WordTemp;
+
+} 
 
 /* --------------------------------------------------------------------------------------------------------- */
 /* --------------------------------------------------------------------------------------------------------- */
